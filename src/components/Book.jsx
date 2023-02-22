e1aa63e5434c732a66f04a2e3e0b7b752b28570f
ValeriaDiaz <valeriadiamon@gmail.com>
2023-02-22 17:52:30 -0600

    getting book image

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,8 +1,12 @@
+import noImage from "../assets/noImage.png"
+
 const Book = ({data}) => {
   return (
     <div>
         {data.map(item => (
           <div key={item.id}>
+            <img src={ item.volumeInfo?.imageLinks.smallThumbnail
+ || noImage} alt="" />
             <p>{item.volumeInfo.title}</p>
           </div>
         ) )}
