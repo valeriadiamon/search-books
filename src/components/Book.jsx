b587e4d8c6d2bd8b94d50573a17042371b8df538
ValeriaDiaz <valeriadiamon@gmail.com>
2023-02-22 18:07:56 -0600

    style added to the books display

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,16 +1,25 @@
 import noImage from "../assets/noImage.png"
+import "./style.css"
 
 const Book = ({data}) => {
   return (
-    <div>
-        {data.map(item => (
-          <div key={item.id}>
-            <img src={ item.volumeInfo?.imageLinks.smallThumbnail
+    <div className="container">
+      {data.map(item => (
+      <div key={item.id} className="perspect">
+        <div className="book">
+          <div className="cover">
+            <img src={ item.volumeInfo.imageLinks?.thumbnail
  || noImage} alt="" />
-            <p>{item.volumeInfo.title}</p>
           </div>
-        ) )}
+        <div className="left-side">
+          <h2>
+            <span>{item.volumeInfo.title}</span>
+          </h2>
+        </div>
+      </div>
     </div>
+    ) )}
+  </div>
   )
 }
 
